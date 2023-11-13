7260d920301abff78c406f359b64e77f90228cb7
omararabi100 <omararabi100@gmail.com>
2023-11-13 18:00:39 +0200

    router working

--- eCommerce/src/components/NavBar.jsx
+++ eCommerce/src/components/NavBar.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
-import AppRoutes from './components/AppRoutes';
+import AppRoutes from './AppRoutes';
 
 function NavBar() {
-    const [count, setCount] = useState(0)
 
     return (
         <div className='nav-bar'>
