acc76b998730ba59bbd035b2644616be726e2272
omararabi100 <omararabi100@gmail.com>
2023-11-22 11:15:26 +0200

    redeem code done

---
+++ eCommerce/src/codes.js
@@ -0,0 +1,56 @@
+export const codes = [
+    {
+        code: "giveme200pls",
+        type: "gift",
+        value: 200,
+        used: false
+    },
+    {
+        code: "reduce15pls",
+        type: "discount",
+        value: 15,
+        used: false
+    },
+    {
+        code: "giveme2000pls",
+        type: "gift",
+        value: 2000,
+        used: false
+    },
+    {
+        code: "reduce30pls",
+        type: "discount",
+        value: 30,
+        used: false
+    },
+    {
+        code: "giveme200pls",
+        type: "gift",
+        value: 200,
+        used: false
+    },
+    {
+        code: "reduce80pls",
+        type: "discount",
+        value: 80,
+        used: false
+    },
+    {
+        code: "giveme5000pls",
+        type: "gift",
+        value: 5000,
+        used: false
+    },
+    {
+        code: "reduceallpls",
+        type: "discount",
+        value: 100,
+        used: false
+    },
+    {
+        code: "givemealotpls",
+        type: "gift",
+        value: 100000,
+        used: false
+    },
+]
--- eCommerce/src/components/Cart.jsx
+++ eCommerce/src/components/Cart.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
-import Item from "./Item";
+import { codes } from "../codes"
 
 function Cart({cartItems, updateItemQuantity, wallet, addMoneyToWallet, emptyCart}) {
     const [totalPrice, setTotalPrice] = useState(0)
+    const [discount, setDiscount] = useState(0)
+    const [code, setCode] = useState("")
+    const [key, setKey] = useState(0)
+    const [msg, setMsg] = useState("")
     useEffect(() => {
         calcTotal(cartItems)
     }, []);
@@ -16,15 +20,49 @@ function Cart({cartItems, updateItemQuantity, wallet, addMoneyToWallet, emptyCar
             total = total + (item.price * item.quantity)
         })
         setTotalPrice(total)
+        setKey(key + 1)
     }
     const addMoney = (amount) => {
         addMoneyToWallet(amount)
     }
     const buy = () => {
-        if (wallet >= totalPrice) {
-            addMoney(-totalPrice)
+        if (wallet >= (totalPrice*((100-discount)/100))) {
+            addMoney(-(totalPrice*((100-discount)/100)))
             emptyCart()
             setTotalPrice(0)
+            setDiscount(0)
+        }
+    }
+    const redeem = () => {
+        setMsg("")
+        const codeIndex = codes.findIndex((icode) => code === icode.code)
+        if (codes[codeIndex]) {
+            const type = codes[codeIndex].type
+            const value = codes[codeIndex].value
+            if (codes[codeIndex].used) {
+                setMsg("Code already used")
+            }
+            else if (type == "discount") {
+                getDiscount(value)
+                codes[codeIndex].used = !codes[codeIndex].used
+            }
+            else if (type == "gift") {
+                addMoney(value)
+                codes[codeIndex].used = !codes[codeIndex].used
+            }
+        }
+        else {
+            setMsg("Invalid Code")
+        }
+        
+        calcTotal(cartItems)
+    }
+    const getDiscount = (value) => {
+        if ((discount + value) <= 100) {
+            setDiscount(discount + value)
+        }
+        else {
+            setDiscount(100)
         }
     }
     return (
@@ -42,9 +80,14 @@ function Cart({cartItems, updateItemQuantity, wallet, addMoneyToWallet, emptyCar
             </div>
             <div className="cart-container-right">
                 <h1>Total Price: ${totalPrice}</h1>
-                <h1>wallet: ${wallet}</h1>
-                <button onClick={() => addMoney(10000)}>Add $10000</button>
-                <button onClick={buy}>Buy</button>
+                <h1>Wallet: ${wallet}</h1>
+                <h1>Discount: {discount}%</h1>
+                <h1>Discounted Price: ${(totalPrice*((100-discount)/100))}</h1>
+                <label htmlFor="input-code">Code</label>
+                <input type="text" value={code} onChange={(e) => {setCode(e.target.value)}}/> <br />
+                <button className="redeem-btn" onClick={redeem}>Redeem</button>
+                <button className="buy-btn" onClick={buy}>Buy</button>
+                <p>{msg}</p>
             </div>
         </div>
     )
